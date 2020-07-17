3e4e2190c5d50620d435898486e206b8ffb7fb23
Edward Stoner <edwardlstoner@gmail.com>
2020-07-17 17:57:51 -0600

    Add more info on resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,8 +1,16 @@
 import React from "react"
 import Layout from "../components/layout"
+import { Link } from "gatsby"
+import ReadMore from "../components/read-more"
 
 const Resume = () => {
-  return <div>hi</div>
+  return (
+    <Layout>
+      <h1>About Me</h1>
+      <p>This is my resume</p>
+      <ReadMore to="/">&larr; back to home</ReadMore>
+    </Layout>
+  )
 }
 
 export default Resume
