4db6b2c65078ac285d2383d928aa488cfa5aa33c
Edward Stoner <edwardlstoner@gmail.com>
2020-07-17 17:57:56 -0600

    Add post

---
+++ src/templates/post.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import { css } from "@emotion/core"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import Layout from "../components/layout"
+import ReadMore from "../components/read-more"
+
+export const query = graphql`
+  query($slug: String!) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+      }
+      body
+    }
+  }
+`
+
+const PostTemplate = ({ data: { mdx: post } }) => (
+  <Layout>
+    <h1>{post.frontmatter.title}</h1>
+
+    <MDXRenderer>{post.body}</MDXRenderer>
+    <ReadMore to="/blog">&larr; back to all posts</ReadMore>
+  </Layout>
+)
+
+export default PostTemplate
