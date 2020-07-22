5f0c35bb204653b39a3454f21335fa19e67eb6e4
Edward Stoner <edwardlstoner@gmail.com>
2020-07-22 13:14:40 -0600

    Add combined SVG

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,16 @@
 import React, { Component } from "react"
-import { Link, graphql } from "gatsby"
-import { css, jsx, keyframes } from "@emotion/core"
-import styled from "@emotion/styled"
-import Img from "gatsby-image"
-
+import { graphql } from "gatsby"
+import { css } from "@emotion/core"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Mountains from "../../assets/mtns.svg"
-import Cloud from "../components/cloud"
 import Sun from "../components/sun"
 import Star from "../components/star"
 import Moon from "../components/moon"
 import gsap, { CSSPlugin } from "gsap"
 import { MotionPathPlugin } from "gsap/MotionPathPlugin"
-
-import { nominalTypeHack } from "prop-types"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
+import MtnsSun from "../components/mtns-sun"
 
 if (typeof window !== `undefined`) {
   gsap.registerPlugin(ScrollTrigger)
@@ -24,19 +19,10 @@ if (typeof window !== `undefined`) {
 
 gsap.registerPlugin(MotionPathPlugin)
 
-const plugins = [CSSPlugin, MotionPathPlugin]
-
-const PHero = props => (
-  <p
-    css={css`
-      margin-bottom: 1rem;
-      color: #222;
-    `}
-    {...props}
-  />
-)
+// const plugins = [CSSPlugin, MotionPathPlugin]
 
 const H1Hero = props => (
+  //eslint-disable-next-line
   <h1
     css={css`
       margin-bottom: 1.5rem;
@@ -47,6 +33,7 @@ const H1Hero = props => (
 )
 
 const H3Hero = props => (
+  //eslint-disable-next-line
   <h3
     css={css`
       margin-bottom: 1.5rem;
@@ -58,49 +45,11 @@ const H3Hero = props => (
   />
 )
 
-// const opacity = keyframes`
-//   from {
-//         transform: translateX(-50px);
-
-//     opacity: 0;
-//     color: lightgray;
-//   }
-
-//   20% {
-//     transform: translateX(-10px);
-//   }
-
-//   to {
-//     opacity: 1;
-//     transform: translateX(0);
-//   }
-// `
-
-// const animationSmallText = keyframes`
-// from {
-//   opacity: 0
-// }
-//   50% {
-//         transform: translateX(-50px);
-
-//     opacity: 0;
-//     color: lightgray;
-//   }
-
-//   80% {
-//     transform: translateX(-10px);
-//   }
-
-//   to {
-//     opacity: 1;
-//     transform: translateX(0);
-//   }
-// `
+console.log(window)
 
 class IndexPage extends Component {
   constructor(props) {
     super(props)
-    // reference to the DOM node
     this.sun = null
     this.cloud = null
     this.star = null
@@ -110,8 +59,6 @@ class IndexPage extends Component {
     this.state = {
       opacity: 0,
     }
-
-    // reference to the animation
   }
 
   darkenBackground = amount => {
@@ -119,7 +66,6 @@ class IndexPage extends Component {
   }
 
   lightenBackground = amount => {
-    console.log(amount.progress)
     let opacity
     if (amount.progress > 0.5) {
       opacity = 0
@@ -130,13 +76,6 @@ class IndexPage extends Component {
   }
 
   componentDidMount() {
-    // use the node ref to create the animation
-    // ScrollTrigger.create({
-    //   trigger: ".scene",
-    //   start: "top 150px",
-    //   end: "bottom 550px",
-    //   pin: this.sun,
-    // })
     ScrollTrigger.create({
       trigger: ".scene",
       start: "top 100px",
@@ -165,29 +104,6 @@ class IndexPage extends Component {
         ease: "power3.inOut",
       },
       "-=.75"
-    )
-    tl.fromTo(
-      this.sun,
-      1,
-      {
-        y: 300,
-        ease: "back.out(1.7)",
-      },
-      {
-        y: 0,
-        ease: "back.out(1.7)",
-      },
-      "-=.5"
-    )
-    tl.to(
-      this.sun,
-      15,
-      {
-        rotation: 180,
-        repeat: -1,
-        ease: "none",
-      },
-      "-=2"
     )
 
     let tl2 = gsap.timeline({
@@ -244,16 +160,6 @@ class IndexPage extends Component {
 
     tl4.from(".hero-three-title", 1, { x: "-20px", opacity: 0.1 }, "-=1.5")
     tl4.from(".hero-three-subtitle", 1, { x: "-20px", opacity: 0.1 }, "-=1")
-    // tl.from(
-    //   this.cloud,
-    //   18,
-    //   {
-    //     x: -1700,
-    //     ease: "none",
-    //     repeat: -1,
-    //   },
-    //   "-=2"
-    // )
   }
   render() {
     return (
@@ -263,63 +169,24 @@ class IndexPage extends Component {
         <div
           className="scene"
           css={css`
-            height: 700px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            /* align-items: flex-end; */
           `}
         >
-          <Sun ref={div => (this.sun = div)} className="sun" />
-          <Mountains
-            css={css`
-              position: absolute;
-              width: 100%;
-              right: 0%;
-              margin-left: 4em;
-              margin-top: 10em;
-              max-width: 955px;
-            `}
-          />
-
-          {/* <Cloud ref={div => (this.cloud = div)} /> */}
-
-          <H1Hero
-            className="hero-title"
-            // css={css`
-            //   animation: ${opacity} 1s ease-in-out 1;
-            // `}
-          >
-            Hi! I'm Ed.
-          </H1Hero>
-
           <div
-            className="text-image-container"
             css={css`
-              display: flex;
+              width: 35%;
             `}
           >
-            <div
-              className="text-container"
-              css={css`
-                margin-right: 3em;
-              `}
-            >
-              <H3Hero
-                className="hero-subtitle"
-                css={css`
-                  width: 15em;
-                `}
-              >
-                I'm a software developer living in Denver, Colorado. I work in
-                React, React Native, GraphQL and Node.
-              </H3Hero>
-            </div>
-            <div>
-              {/* <Img
-            css={css`
-              border-radius: 1em;
-            `}
-            fixed={data.file.childImageSharp.fixed}
-          /> */}
-            </div>
+            <H1Hero className="hero-title">Hi! I'm Ed.</H1Hero>
+            <H3Hero className="hero-subtitle">
+              I'm a software developer living in Denver, Colorado. I work in
+              React, React Native, GraphQL and Node.
+            </H3Hero>
           </div>
+          <MtnsSun />
         </div>
 
         <div
@@ -344,18 +211,18 @@ class IndexPage extends Component {
           >
             <Star
               ref={div => (this.star = div)}
-              marginLeft="5%"
-              marginTop="90px"
+              marginLeft="30%"
+              marginTop="-40px"
             />
             <Star
               ref={div => (this.star1 = div)}
-              marginLeft="8%"
-              marginTop="0px"
+              marginLeft="45%"
+              marginTop="0"
             />
             <Star
               ref={div => (this.star2 = div)}
-              marginLeft="8%"
-              marginTop="110px"
+              marginLeft="45%"
+              marginTop="7%"
             />
             <Moon ref={div => (this.moon = div)} />
           </div>
